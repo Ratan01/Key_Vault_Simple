416f61925edf26debc974c7b9fbad94a59a2e248
Ratan01 <ratanrudra010@gmail.com>
2023-09-11 15:13:51 +0530

    new file updated

--- key_vault/key_vault.tf
+++ key_vault/key_vault.tf
@@ -1,44 +1,47 @@
 provider "azurerm" {
-  features {
-    key_vault {
-      #purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
+    features {
+      
     }
-  }
 }
-
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "keyvaultrk-resources"
+  name     = "keyrk-resources"
   location = "West Europe"
 }
 
-resource "azurerm_key_vault" "kev" {
-  name                        = "keyvaultrk"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+resource "azurerm_key_vault" "kav" {
+  name                = "rkkeyvault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
 
-  sku_name = "standard"
+resource "azurerm_key_vault_access_policy" "kvap" {
+  key_vault_id = azurerm_key_vault.kav.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+  key_permissions = [
+    "Get",
+  ]
 
-    key_permissions = [
-      "Get",
-    ]
+  secret_permissions = [
+    "Get",
+  ]
+}
 
-    secret_permissions = [
-      "Get",
-    ]
+data "azuread_service_principal" "sp" {
+  display_name = "rk-app"
+}
 
-    storage_permissions = [
-      "Get",
-    ]
-  }
+resource "azurerm_key_vault_access_policy" "rk-principal" {
+  key_vault_id = azurerm_key_vault.kav.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.sp.object_id
+
+  key_permissions = [
+    "Get", "List", "Encrypt", "Decrypt"
+  ]
 }
