5a81b24114bf9396f88be1a4f0ba07ba1174c4e2
Ratan01 <ratanrudra010@gmail.com>
2023-09-11 15:31:38 +0530

    file updated

--- key_vault/key_vault.tf
+++ key_vault/key_vault.tf
@@ -1,64 +1,44 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
-  location = var.resource_group_location
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
 
-resource "random_string" "azurerm_key_vault_name" {
-  length  = 13
-  lower   = true
-  numeric = false
-  special = false
-  upper   = false
+resource "azurerm_resource_group" "rg" {
+  name     = "vak-resources"
+  location = "West Europe"
 }
 
-locals {
-  current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
-}
+resource "azurerm_key_vault" "vaka" {
+  name                        = "rkkeyvault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
 
-resource "azurerm_key_vault" "vault" {
-  name                       = coalesce(var.vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = var.sku_name
-  soft_delete_retention_days = 7
+  sku_name = "standard"
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = local.current_user_id
+    object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions    = var.key_permissions
-    secret_permissions = var.secret_permissions
-  }
-}
+    key_permissions = [
+      "Get",
+    ]
 
-resource "random_string" "azurerm_key_vault_key_name" {
-  length  = 13
-  lower   = true
-  numeric = false
-  special = false
-  upper   = false
-}
+    secret_permissions = [
+      "Get",
+    ]
 
-resource "azurerm_key_vault_key" "key" {
-  name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
-
-  key_vault_id = azurerm_key_vault.vault.id
-  key_type     = var.key_type
-  key_size     = var.key_size
-  key_opts     = var.key_ops
-
-  rotation_policy {
-    automatic {
-      time_before_expiry = "P30D"
-    }
-
-    expire_after         = "P90D"
-    notify_before_expiry = "P29D"
+    storage_permissions = [
+      "Get",
+    ]
   }
 }
